d1ce87902fde24afafca88dbfd5aafdc1f9c2c16
Aditu2016Catur <adi10_00@yahoo.com>
2017-01-22 20:11:57 +0200

    Work in progress topbar

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hello World!
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prima Pagina</title>
+	<?php include '/pages/head.html'; ?>
+</head>
+<body>
+<?php include '/pages/header.html'; ?>
+</body>
+</html>
